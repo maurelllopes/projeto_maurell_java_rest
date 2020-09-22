0478627d4691b735d2b036cc250d553f5672c609
Alexandre Maurell <maurelllopes@mac.com>
2020-09-22 07:16:09 -0300

    Testando a primeira classe de dominio'

--- src/main/java/com/maurelllopes/projeto_maurell/resource/Categorias.java
+++ src/main/java/com/maurelllopes/projeto_maurell/resource/Categorias.java
@@ -1,16 +1,28 @@
 package com.maurelllopes.projeto_maurell.resource;
 
+import com.maurelllopes.projeto_maurell.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class Categorias {
 
-    @RequestMapping(method=RequestMethod.GET)
-    public String listar(){
-        return "REST";
-    }
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
 
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+
+    }
 }
